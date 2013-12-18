6139d39f2ed429d07c4901ced4e376ecfd2a16e5
Shi Zhang <shizhang@jhu.edu>
2013-12-17 20:03:00 -0500

    init

---
+++ station/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from station import views
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^courses/', views.courses, name='course'),
+        url(r'^school/', views.school, name='school'),
+        url(r'^category/', views.category, name='category'),
+        url(r'^about/', views.about, name='about'))
